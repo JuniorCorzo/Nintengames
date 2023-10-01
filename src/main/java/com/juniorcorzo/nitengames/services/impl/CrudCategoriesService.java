b789b3424cea6be3d65b1576e768a80a3e3a58a0
JuniorCorzo <josedanielmmf@gmail.com>
2023-10-01 11:13:08 -0500

    Añadido el servicio CRUD categories

---
+++ src/main/java/com/juniorcorzo/nitengames/services/impl/CrudCategoriesService.java
@@ -0,0 +1,38 @@
+package com.juniorcorzo.nitengames.services.impl;
+
+import com.juniorcorzo.nitengames.entity.Categories;
+import com.juniorcorzo.nitengames.repository.CategoriesRepository;
+import com.juniorcorzo.nitengames.services.CrudBasicService;
+
+import java.util.List;
+
+public class CrudCategoriesService implements CrudBasicService<Categories, Integer> {
+    private CategoriesRepository categoriesRepository;
+
+    @Override
+    public Categories create(Categories entity) {
+        return categoriesRepository.save(entity);
+    }
+
+    @Override
+    public Categories update(Categories entity, Integer id) {
+        if (!categoriesRepository.existsById(id)) throw new RuntimeException("Categoria no encontrada, ID: " + id);
+        return categoriesRepository.save(entity);
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        categoriesRepository.deleteById(id);
+        return !categoriesRepository.existsById(id);
+    }
+
+    @Override
+    public List<Categories> getAll() {
+        return categoriesRepository.findAll();
+    }
+
+    @Override
+    public Categories getById(Integer id) {
+        return categoriesRepository.findById(id).orElse(null);
+    }
+}
